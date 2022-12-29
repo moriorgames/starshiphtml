a7c0c0bde39a746ce8d1a7f02c67626430e87e4c
Morior <jordi.grados@escapadarural.com>
2022-12-29 18:03:14 +0100

    Move files to a presentation layer

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById('miCanvas');
 const ctx = canvas.getContext('2d');
 
 import updateGameState from './gameState.js';
-import drawBackground from './background.js';
-import drawGameElements from './gameElements.js';
+import drawBackground from './presenter/background.js';
+import drawGameElements from './presenter/gameElements.js';
 
 function gameLoop() {
     updateGameState();
