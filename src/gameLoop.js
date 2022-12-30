9c7061794047fad068bcbd856d27a1146a9f7d78
Morior <jordi.grados@escapadarural.com>
2022-12-30 11:58:48 +0100

    Added keyboard events

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -3,12 +3,14 @@ const ctx = canvas.getContext('2d');
 
 import GridFactory from './factory/gridFactory.js';
 import Player from './entity/player.js';
+import PlayerController from './controller/playerController.js';
 import updateGameState from './gameState.js';
 import drawBackground from './presenter/background.js';
 import GameElements from './presenter/gameElements.js';
 
 const player = new Player;
 const grid = (new GridFactory).create();
+const playerController = new PlayerController(player);
 const gameElements = new GameElements;
 
 function gameLoop() {
@@ -22,3 +24,18 @@ function gameLoop() {
 }
 
 gameLoop();
+
+window.addEventListener('keydown', (event) => {
+    if (event.code == 'ArrowLeft') {
+        playerController.moveLeft();
+    }
+    if (event.code == 'ArrowRight') {
+        playerController.moveRight();
+    }
+    if (event.code == 'ArrowUp') {
+        playerController.moveUp();
+    }
+    if (event.code == 'ArrowDown') {
+        playerController.moveDown();
+    }
+}, true);
