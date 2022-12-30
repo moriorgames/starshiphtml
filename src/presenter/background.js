81ee0cc438772ab2eac2fac9c7630fbf32017f64
Morior <jordi.grados@escapadarural.com>
2022-12-30 16:58:44 +0100

    Added a cool background

--- src/presenter/background.js
+++ src/presenter/background.js
@@ -1,11 +1,11 @@
 export default function drawBackground(score, canvas, ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, 250);
+    let img = document.getElementById("background");
+    ctx.drawImage(img, 0, 0);
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 250, canvas.width, canvas.height);
+    ctx.fillStyle = 'DarkCyan';
+    ctx.fillRect(0, 450, canvas.width, canvas.height);
 
     ctx.font = '18px serif';
     ctx.fillStyle = 'white';
